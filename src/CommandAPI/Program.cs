04a9529b8fc0e44a25d36261916bc4503987af3c
Nkem Okwuone <nkemokwuone@Nkems-Macbook.local>
2022-06-12 07:43:13 +0100

    Adding postgres connection

--- src/CommandAPI/Program.cs
+++ src/CommandAPI/Program.cs
@@ -1,4 +1,6 @@
 using CommandAPI.Data;
+using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,11 +10,12 @@ builder.Services.AddControllers();
 builder.Services.AddScoped<ICommandAPIRepo, MockCommandAPIRepo >();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-/*builder.Services.AddSwaggerGen();
-builder.Services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+/*builder.Services.AddSwaggerGen();*/
 
-*/
+builder.Services.AddDbContext<CommandDBContext>(options =>
+                options.UseNpgsql(builder.Configuration.GetConnectionString("Postgres")));
+
+
 
 var app = builder.Build();
 // Configure the HTTP request pipeline.
